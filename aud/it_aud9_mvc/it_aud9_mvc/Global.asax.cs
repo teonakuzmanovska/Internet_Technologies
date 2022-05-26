37986edf67de5c80c6f7ba878eab6cd767f8146e
teonakuzmanovska <teona.kuzmanovska@students.finki.ukim.mk>
2022-05-26 14:27:21 +0200

    updated it_aud9_mvc
    added ApiController and changed url and method in bootbox

--- aud/it_aud9_mvc/it_aud9_mvc/Global.asax.cs
+++ aud/it_aud9_mvc/it_aud9_mvc/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.Http;
 
 namespace it_aud9_mvc
 {
@@ -12,6 +13,7 @@ namespace it_aud9_mvc
     {
         protected void Application_Start()
         {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
